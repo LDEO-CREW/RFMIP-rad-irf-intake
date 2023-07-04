cb3852b2393e1d2c41dfb21e98be854c57fdd9c5
Robert Pincus <Robert.Pincus@columbia.edu>
2023-07-04 11:57:28 -0400

    Add continuous integration; update diagram

--- report_availability.py
+++ report_availability.py
@@ -1,18 +1,22 @@
 #! /usr/env/python 
 from intake import open_catalog
-import warnings
 from xarray import SerializationWarning
+import warnings
+import sys
 
+# xarray throws many SerializationWarning due to slightly different missing values
 warnings.simplefilter("ignore", SerializationWarning)
 
+
+failed = False
 cat = open_catalog("main.yml")
 
 print("Inputs")
 try:
   x = cat.inputs.to_dask() 
 except: 
-    print(f'inputs not available')
-    print(cat.inputs.urlpath)
+    failed = True
+    print(f'inputs not available ({at.inputs.urlpath})')
 
 for t in ["benchmark", "parameterized"]: 
     for m in list(cat[t]):
@@ -22,6 +26,7 @@ for t in ["benchmark", "parameterized"]:
                 try: 
                     x = cat[t][m][v][f].to_dask()
                 except: 
+                    failed = True
                     print(f'{m} {v} {f} not available ({cat[t][m][v][f].urlpath})')
 
-
+sys.exit(1) if failed else sys.exit(0)
